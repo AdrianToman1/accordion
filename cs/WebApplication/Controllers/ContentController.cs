c6bba92cd5f6d645496ff5db0eab9fab0a0625e7
Adrian Toman <adrian@adriantoman.com>
2018-12-29 15:08:24 +1100

    Add content endpoint to webserver

---
+++ cs/WebApplication/Controllers/ContentController.cs
@@ -0,0 +1,24 @@
+﻿using HTMLInterpreter;
+using HTMLRepo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class ContentController : Controller
+    {
+        public IActionResult Index(string id)
+        {
+            IHTMLInterpreter htmlInterpreter = new HTMLInterpreter.HTMLInterpreter();
+            IHTMLRepo htmlRepo = new HTMLRepo.HTMLRepo();
+
+            string html = htmlRepo.GetHTMLPage(id + ".html");
+
+            if (string.IsNullOrWhiteSpace(html))
+            {
+                return NotFound();
+            }
+
+            return Content(htmlInterpreter.ApplyPermissions(html), "text/html");
+        }
+    }
+}
